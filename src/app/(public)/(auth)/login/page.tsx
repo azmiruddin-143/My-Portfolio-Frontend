ab0cf8a48e119d74a5644f871d957435da5c2991
rjazmirkhan <bangladeshfreelancerazmir@gmail.com>
2025-10-04 07:56:27 +0600

    Register Complete

---
+++ src/app/(public)/(auth)/login/page.tsx
@@ -0,0 +1,10 @@
+import LoginForm from '@/components/modules/Auth/LoginForm';
+import React from 'react';
+
+const LoginPage = () => {
+    return (
+        <LoginForm></LoginForm>
+    );
+};
+
+export default LoginPage;
---
+++ src/app/(public)/(auth)/register/page.tsx
@@ -0,0 +1,10 @@
+import RegisterForm from '@/components/modules/Auth/RegisterForm';
+import React from 'react';
+
+const Registerpage = () => {
+    return (
+        <RegisterForm></RegisterForm>
+    );
+};
+
+export default Registerpage;
